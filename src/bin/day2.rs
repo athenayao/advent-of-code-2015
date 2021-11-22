45482a139f7482d1b396093a3d599c4d396650b6
Athena Yao <athena@discordapp.com>
2021-11-22 09:48:32 -0800

    [day 2] Part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,4 +1,4 @@
-fn calculate(input: &str) -> i32 {
+fn calculate_paper(input: &str) -> i32 {
     let v: Vec<i32> = input.trim().split("x").map(|val| val.parse().unwrap() ).collect();
     let mut i = 0;
     let mut min_area = i32::MAX;
@@ -18,11 +18,23 @@ fn calculate(input: &str) -> i32 {
     total + min_area
 }
 
+fn calculate_ribbon(input: &str) -> i32 {
+    let mut v: Vec<i32> = input.trim().split("x").map(|val| val.parse().unwrap() ).collect();
+    v.sort();
+
+    let mut i = 0;
+    let mut total = 0;
+    let smallest_perimeter = 2 * v[0] + 2 * v[1];
+    let ribbon = v[0] * v[1] * v[2];
+    smallest_perimeter + ribbon
+
+}
+
 fn main() {
     let input = include_str!("day2.txt");
     let mut subtotal = 0;
     for line in input.split("\n") {
-        subtotal += calculate(line);
+        subtotal += calculate_ribbon(line);
     }
     println!("ANSWER: {}", subtotal)
 }
@@ -31,8 +43,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_calculate() {
-        assert_eq!(calculate("2x3x4"), 58);
-        assert_eq!(calculate("1x1x10"), 43);
+    fn test_calculate_paper() {
+        assert_eq!(calculate_paper("2x3x4"), 58);
+        assert_eq!(calculate_paper("1x1x10"), 43);
+    }
+
+    fn test_calculate_ribbon() {
+        assert_eq!(calculate_ribbon("2x3x4"), 34);
+        assert_eq!(calculate_ribbon("1x1x10"), 14);
     }
 }
