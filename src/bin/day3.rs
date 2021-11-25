cce2c38e13c16e0c4b5dbdc23aa4e6f23ad3282b
Athena Yao <athena@discordapp.com>
2021-11-24 23:23:05 -0800

    [day 3] Part 2. Naive approach

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,11 +1,17 @@
 use std::collections::HashSet;
 
 fn count_houses(input: &str) -> usize {
-    let mut x = 0;
-    let mut y = 0;
-    let mut set = HashSet::new();
-    set.insert(format!("{}:{}", x, y));
-    for c in input.chars() {
+    let mut santa_x = 0;
+    let mut santa_y = 0;
+    let mut robo_x = 0;
+    let mut robo_y = 0;
+
+    let mut santa_set = HashSet::new();
+    let mut robo_set = HashSet::new();
+
+    santa_set.insert(format!("{}:{}", santa_x, santa_y));
+    robo_set.insert(format!("{}:{}", robo_x, robo_y));
+    for (i, c) in input.chars().enumerate() {
         let (delta_x, delta_y) = match c {
             '>' => (1, 0),
             '<' => (-1, 0),
@@ -13,12 +19,20 @@ fn count_houses(input: &str) -> usize {
             'v' => (0, 1),
             _ => (0, 0),
         };
-        x = x + delta_x;
-        y = y + delta_y;
-        set.insert(format!("{}:{}", x, y));
+        if i % 2 == 0 {
+            santa_x = santa_x + delta_x;
+            santa_y = santa_y + delta_y;
+            santa_set.insert(format!("{}:{}", santa_x, santa_y));
+        } else {
+            robo_x = robo_x + delta_x;
+            robo_y = robo_y + delta_y;
+            robo_set.insert(format!("{}:{}", robo_x, robo_y));
+        }
     }
-    set.len()
+    let the_union: HashSet<_> = santa_set.union(&robo_set).collect();
+    the_union.len()
 }
+
 
 fn main() {
     let input = include_str!("day3.txt");
@@ -30,8 +44,8 @@ mod tests {
 
     #[test]
     fn test_count_houses() {
-        assert_eq!(count_houses(">"), 2);
-        assert_eq!(count_houses("^>v<"), 4);
-        assert_eq!(count_houses("^v^v^v^v^v"), 2);
+        assert_eq!(count_houses("^v"), 3);
+        assert_eq!(count_houses("^>v<"), 3);
+        assert_eq!(count_houses("^v^v^v^v^v"), 11);
     }
 }
